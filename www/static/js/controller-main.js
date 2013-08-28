b4040d3f7e8e66811853ce667b6689199d158657
Jake Archibald <jaffathecake@gmail.com>
2013-08-28 10:13:32 +0100

    Controller

--- www/static/js/controller-main.js
+++ www/static/js/controller-main.js
@@ -1,8 +1,27 @@
-var cache = new Cache(
+// this should be here by default
+this.caches = new CacheList();
+
+
+caches.set('core', new Cache('core',
+  '/',
   '/static/js/all.js',
   '/static/css/all.css',
   '/static/css/fonts/oswald.ttf',
   '/static/css/imgs/sprites1.png',
   '/static/tutorial-sprite.png',
   '/static/favicon.ico'
-);
+));
+
+this.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match('core', event.request.url).catch(function() {
+      var response = new SameOriginResponse();
+      response.setBody(
+        new Blob(['Cache fetch failed'], {
+          type: 'text/plain'
+        })
+      );
+      return response;
+    })
+  );
+});
